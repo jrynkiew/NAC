0e47dfd71901e302fd97fe062aa928831cb66fab
JRPC <jrynk@hotmail.com>
2023-01-30 18:47:49 +0100

    partial commit

--- build/env.sh
+++ build/env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Environment variables
-red=`tput setaf 1`
-purple=`tput setaf 93`
-blue=`tput setaf 21`
-green=`tput setaf 2`
-gold=`tput setaf 214`
-reset=`tput sgr0`
+export red=`tput setaf 1`
+export purple=`tput setaf 93`
+export blue=`tput setaf 21`
+export green=`tput setaf 2`
+export gold=`tput setaf 214`
+export reset=`tput sgr0`
 
 export jrpc_beta_v2=$PWD
 
--- install.sh
+++ install.sh
@@ -5,10 +5,44 @@
 banner
 
 # Launch Build
-docker-compose -p jrpc -f $jrpc_beta_v2/build/docker-compose.yaml build
+docker-compose -p jrpc -f $jrpc_beta_v2/build/docker-compose.yaml build --parallel
 
 # Launch Docker
 docker-compose -p jrpc -f $jrpc_beta_v2/build/docker-compose.yaml up -d
+
+echo "
+┍━━━━━━━━━━━━━━━━━━━━ ⋆⋅☆⋅⋆ ━━━━━━━━━━━━━━━━━━━━┑"
+echo "
+Please finish the installation on ${green}http://localhost:8080${reset}"
+echo "You will need to set up the following:
+"
+echo "  - ${gold}Admin Account${reset}  -  set to u:admin p:admin"
+echo "  - ${gold}Server URL${reset}  -  set to http://jenkins:8080"
+echo "  - ${gold}Install plugins${reset}  -  select any you want"
+
+echo "
+You will be asked to provide a 'one-time' Jenkins administrator password."
+
+echo "
+Your password is 
+      ${red}
+        $(cat $jrpc_beta_v2/build/jenkins/data/secrets/initialAdminPassword)
+      ${reset}" 
+
+echo "┕━━━━━━━━━━━━━━━━━━━━ ⋆⋅☆⋅⋆ ━━━━━━━━━━━━━━━━━━━━┙
+"
+
+while :
+do
+  # Getting necessary user input
+  read -p "When you have completed the Jenkins node setup, press any key" CONTINUE
+
+  case $CONTINUE in
+    *) # Build IoTeX Full Node
+      cd $jrpc_beta/external/JRPC/
+      bash $jrpc_beta/external/JRPC/install.sh 
+  esac
+done
 
 # Exit Docker
 docker-compose -p jrpc -f $jrpc_beta_v2/build/docker-compose.yaml down
