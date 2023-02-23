5d2fc7b5e10c02b22721caebcdff7354fade8bb6
JRPC <jrynk@hotmail.com>
2023-02-23 10:00:22 +0100

    fixing main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@
 // ref: https://gist.github.com/SuperV1234/5c5ad838fe5fe1bf54f9
 
 
-
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#else
+#include "glad/glad.h"
+#endif
 #include <renderer.h>
 
 static const struct
@@ -17,43 +23,75 @@ static const struct
     {
         {-0.6f, -0.4f, 1.f, 0.f, 0.f},
         {0.6f, -0.4f, 0.f, 1.f, 0.f},
-        {0.f, 0.6f, 0.f, 0.f, 0.5f}};
+        {0.f, 0.6f, 0.f, 0.f, 1.f}};
 
-mat4x4 m, p, mvp;
-float ratio;
+static const char *vertex_shader_text =
+    "uniform mat4 MVP;\n"
+    "attribute vec3 vCol;\n"
+    "attribute vec2 vPos;\n"
+    "varying vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+    "    color = vCol;\n"
+    "}\n";
+
+#ifdef __EMSCRIPTEN__
+static const char *fragment_shader_text =
+	"precision mediump float;\n"
+    "varying vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_FragColor = vec4(color, 1.0);\n"
+    "}\n";
+#else
+static const char *fragment_shader_text =
+    "varying vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_FragColor = vec4(color, 1.0);\n"
+    "}\n";
+#endif
+
+static void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
 static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
-    if (key == GLFW_KEY_LEFT  && action == GLFW_REPEAT)
-    {
-        fprintf(stdout, "Left key pressed", NULL);
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
-    }
-    if (key == GLFW_KEY_RIGHT && action == GLFW_REPEAT)
-    {
-        fprintf(stdout, "Left key pressed", NULL);
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, -(float)glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
-    }
 }
 
 std::function<void()> loop;
 void main_loop() { loop(); }
 
+void check_error(GLuint shader)
+{
+    GLint result;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        GLint log_length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+        std::vector<GLchar> log(log_length);
+
+        GLsizei length;
+        glGetShaderInfoLog(shader, log.size(), &length, log.data());
+
+        error_callback(0, log.data());
+    }
+}
+
 int main(void)
 {
-    glfwSetErrorCallback(g_sRenderer->error_callback);
+    GLint mvp_location, vpos_location, vcol_location;
+    glfwSetErrorCallback(error_callback);
     if (!glfwInit())
         exit(EXIT_FAILURE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
     auto window = glfwCreateWindow(1280, 720, "Simple example", NULL, NULL);
     if (!window)
     {
@@ -69,28 +107,26 @@ int main(void)
     glfwSwapInterval(1);
     printf("%s\n", glGetString(GL_VERSION));
 
-    //imgui
     g_sRenderer->Initialize(window);
 
+    glEnable(GL_CULL_FACE);
 
+    printf("Use Modern OpenGL (with shaders)\n");
     // NOTE: OpenGL error checks have been omitted for brevity
-    GLint mvp_location, vpos_location, vcol_location;
     GLuint vertex_buffer;
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    const char* vst = g_sRenderer->GetVertexShaderText();
-    const char* fst = g_sRenderer->GetFragmentShaderText();
     auto vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vst, NULL);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
     glCompileShader(vertex_shader);
-    g_sRenderer->check_error(vertex_shader);
+    check_error(vertex_shader);
 
     auto fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fst, NULL);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
     glCompileShader(fragment_shader);
-    g_sRenderer->check_error(fragment_shader);
+    check_error(fragment_shader);
 
     auto program = glCreateProgram();
     glAttachShader(program, vertex_shader);
@@ -107,14 +143,17 @@ int main(void)
                           sizeof(vertices[0]), (void *)(sizeof(float) * 2));
 
     loop = [&] {
-        
+        float ratio;
         int width, height;
-        
+        mat4x4 m, p, mvp;
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float)height;
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
-        
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)mvp);
         glDrawArrays(GL_TRIANGLES, 0, 3);
