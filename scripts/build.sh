b9cf407bc0f788694ebe4887b0f6dd506f755c34
JRPC <jrynk@hotmail.com>
2023-04-19 00:44:57 +0200

    adding web3.js library support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 if [ "$BUILD_TARGET" = "web" ]
 then
+    #remove existing node_modules (if any)
+    rm -rf /build/project/external/web3.js/node_modules/  
+
+    #set up nvm
+    cd /build/.nvm
+    . ./nvm.sh
+
+    #build web3.js
+    cd /build/project/external/web3.js
+    node -v
+    npm install
+    npm run build
+
+    #set up emscripten
     cd /build/emsdk/
     . ./emsdk_env.sh
+
+    #prebuild emscripten libraries
     embuilder build libpng
     embuilder build sdl2
     embuilder build sdl2_image
@@ -11,8 +27,3 @@ fi
 cd /build/project/
 /bin/bash -c "make clean"
 /bin/bash -c "make all -j`nproc`"
-
-# if [ "$BUILD_TARGET" = "web" ]
-# then
-# sed -i 's|index.js|/index.js|' /build/project/generated/web/index.html
-# fi
