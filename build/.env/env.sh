52c3e6143931f0799378ebb05750935b224405cc
Jeremi Rynkiewicz <jeremi@jrpc.pl>
2023-08-18 10:15:18 +0200

    test

--- build/.env/env.sh
+++ build/.env/env.sh
@@ -1,42 +1,7 @@
 #!/bin/bash
 
-# Environment variables
-export red=`tput setaf 1`
-export purple=`tput setaf 93`
-export blue=`tput setaf 21`
-export green=`tput setaf 2`
-export gold=`tput setaf 214`
-export reset=`tput sgr0`
-
 export jrpc_beta_v2=$PWD
 export http_code=0
-
-if [ -x "$(command -v git)" ]; then
-    echo "${gold}git version${reset}"
-    git --version
-else
-    echo "${gold}Installing git${reset}"
-    sudo apt-get install git
-fi
-
-if [ -x "$(command -v docker)" ]; then
-    echo "${gold}docker version${reset}"
-    docker --version
-else
-    echo "${gold}Installing docker${reset}"
-    sudo apt-get install docker
-fi
-
-if [ -x "$(command -v docker-compose)" ]; then
-    echo "${gold}docker-compose version${reset}"
-    docker-compose --version
-else
-    echo "${gold}Installing docker-compose${reset}"
-    sudo apt-get install docker-compose
-fi
-
-echo -e "${gold}Initializing git repo and dependencies${reset}"
-git submodule update --init --recursive
 
 function banner {
 echo "     ____._____________________________  
@@ -48,3 +13,11 @@ echo "     ____._____________________________
 }
 
 export COMMAND="sleep infinity"
+
+if [ "$BUILD_TARGET" = "web" ]
+then
+    export PWD_WEB=$PWD
+    cd /build/emsdk/
+    . ./emsdk_env.sh
+    cd $PWD_WEB
+fi
--- install.sh
+++ install.sh
@@ -1,43 +1,10 @@
+#!/bin/bash
+
 # Initialize Environment
 . ./build/.env/env.sh
 
 # Display Banner
 banner
 
-# Launch Build
-docker-compose -p jrpc -f $jrpc_beta_v2/build/docker/docker-compose.yaml build
-
-mkdir -p $jrpc_beta_v2/build/jenkins/data
-
-# Launch Docker
-docker-compose -p jrpc -f $jrpc_beta_v2/build/docker/docker-compose.yaml up -d jenkins
-
-printf "Waiting for Jenkins to be ready ..."
-until [ ${http_code} -eq 403 ]; do
-    http_code=$(curl -LI --max-time 2 http://localhost:8080 -o /dev/null -w '%{http_code}\n' -s)
-    sleep 3
-    printf "."
-done
-printf " ${green}done${reset}
-"
-
-docker-compose -p jrpc -f $jrpc_beta_v2/build/docker/docker-compose.yaml up -d jrpc-windows-builder jrpc-web-builder jrpc-linux-builder jrpc-windows64-builder
-docker-compose -p jrpc -f $jrpc_beta_v2/build/docker/docker-compose.yaml up -d nginx
-echo "
-┍━━━━━━━━━━━━━━━━━━━━ ⋆⋅☆⋅⋆ ━━━━━━━━━━━━━━━━━━━━┑"
-
-echo "┃ ${green}Jenkins is ready!${reset}                             ┃"
-echo "┃ http://localhost:8080                         ┃"
-
-echo "┃ http://localhost:8080/configuration-as-code/  ┃"
-echo "┃                                               ┃"
-echo "┃ ${green}Python Web Server is ready!${reset}                   ┃"
-echo "┃ http://localhost:80                           ┃"
-
-echo "┕━━━━━━━━━━━━━━━━━━━━ ⋆⋅☆⋅⋆ ━━━━━━━━━━━━━━━━━━━━┙
-"
-
-sleep infinity
-
-# Exit Docker
-docker-compose -p jrpc -f $jrpc_beta_v2/build/docker/docker-compose.yaml down
+/bin/bash -c "make clean"
+/bin/bash -c "make -j`nproc`"
