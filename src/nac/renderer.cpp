17bb986f969b8b097808b6bccd5423e470bba5bf
Jeremi Rynkiewicz <jeremi@jrpc.pl>
2023-08-21 08:15:42 +0200

    test render changes

--- src/nac/renderer.cpp
+++ src/nac/renderer.cpp
@@ -312,9 +312,9 @@ namespace _NAC
 	{
 		ImGui::Render();
 		ImGuiIO& io = ImGui::GetIO(); (void)io;
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
+        // int display_w, display_h;
+        // glfwGetFramebufferSize(window, &display_w, &display_h);
+        // glViewport(0, 0, display_w, display_h);
         // glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     	
