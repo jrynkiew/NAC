54bd425040b888d683920a165764bb67c2359cb5
jeremi <jeremi@jrpc.pl>
2023-08-25 01:39:00 +0200

    testing fragment and shader texts in class

--- main.cpp
+++ main.cpp
@@ -18,34 +18,6 @@
 
 using namespace _NAC;
 
-static const char *vertex_shader_text =
-    "uniform mat4 MVP;\n"
-    "attribute vec3 vCol;\n"
-    "attribute vec2 vPos;\n"
-    "varying vec3 color;\n"
-    "void main()\n"
-    "{\n"
-    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-    "    color = vCol;\n"
-    "}\n";
-
-#ifdef __EMSCRIPTEN__
-static const char *fragment_shader_text =
-	"precision mediump float;\n"
-    "varying vec3 color;\n"
-    "void main()\n"
-    "{\n"
-    "    gl_FragColor = vec4(color, 1.0);\n"
-    "}\n";
-#else
-static const char *fragment_shader_text =
-    "varying vec3 color;\n"
-    "void main()\n"
-    "{\n"
-    "    gl_FragColor = vec4(color, 1.0);\n"
-    "}\n";
-#endif
-
 NAC* nac;
 
 std::function<void()> loop;
