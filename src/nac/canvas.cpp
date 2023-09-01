28c0e6198997271ee6eef12fcb421bea1c8a5612
jeremi <jeremi@jrpc.pl>
2023-09-01 09:58:32 +0200

    testing changing canvas

--- src/nac/canvas.cpp
+++ src/nac/canvas.cpp
@@ -90,7 +90,7 @@ namespace _NAC
         return sizeof(vertices);
     }
 
-    void Canvas::SetVertices(Vertex* vertices) {
+    void Canvas::SetVertices(Vertex* vertices[3]) {
         this->vertices = vertices;
     }
 
