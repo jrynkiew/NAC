600adb55fa98fb7002d2e8ca13fa6097b16736ea
jeremi <jeremi@jrpc.pl>
2023-09-05 19:43:33 +0200

    testing new indices

--- src/nac/canvas.cpp
+++ src/nac/canvas.cpp
@@ -100,8 +100,6 @@ namespace _NAC
         // Set vertex attribute pointers
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-        glEnableVertexAttribArray(1);
 
         // Compile and link the shaders
         vertexShader = glCreateShader(GL_VERTEX_SHADER);
